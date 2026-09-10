c756181192734ab0a167039396bc7f6245342f9b
tatangachikandiwa3087 <25B11CS074@adityauniversity.in>
2026-09-10 10:18:25 +0530

    Time: 215 ms (67.41%) | Memory: 24 MB (32.33%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
@@ -1,22 +1,24 @@
 def canship(weights, days, capacity):
+    #find the days needed to ship all the weights under chosen capacity
     current_weight=0
     days_needed=1
     for weight in weights:
-        if current_weight+weight>capacity:
+        if current_weight+weight<=capacity:
+            current_weight+=weight
+        else:
             days_needed+=1
             current_weight=weight
-        else:
-            current_weight+=weight
+    #Compare days_needed<=days(capacity is a valid choice)
     return days_needed<=days
 
 class Solution:
     def shipWithinDays(self, weights: List[int], days: int) -> int:
-      low=max(weights)
-      high=sum(weights)
-      while low<high:
-        mid=(low+high)//2
-        if canship(weights, days, mid):
-            high=mid
-        else:
-            low=mid+1
-      return low
+        low=max(weights)
+        high=sum(weights)
+        while low<high:
+            mid=(low+high)//2
+            if canship(weights, days, mid):
+                high=mid
+            else:
+                low=mid+1
+        return low
